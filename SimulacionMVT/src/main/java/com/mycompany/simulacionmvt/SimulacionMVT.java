2e9a9ac53781eba1a98d50a2fa753ba1a782d03a
Alberto <alberto.rdz598@gmail.com>
2024-05-23 15:45:13 -0600

    (+) Archivos Completos

---
+++ SimulacionMVT/src/main/java/com/mycompany/simulacionmvt/SimulacionMVT.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.simulacionmvt;
+import Pantalla.Simulacion;
+
+
+/**
+ *
+ * @author alber
+ */
+public class SimulacionMVT {
+
+    public static void main(String[] args) {
+        Simulacion panta = new Simulacion();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+
+}
